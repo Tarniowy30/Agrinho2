03ddfa47e9a14436267efa5bffa2cfc2023aa395
Tarniowy30 <castilho.ana@escola.pr.gov.br>
2025-06-21 22:11:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,41 @@
-// Projeto Agrinho - Festejando a Conexão Campo-Cidade
-// Desenvolvido por [Seu Nome] - #agrinho
+let campoImg, cidadeImg, caminhaoImg, somCaminhao;
+let xCaminhao = 50;
+let viagem = false;
 
-let fundo;
-let tamanhoNuvem = 50;
-let solX = 100;
-let corDoCampo;
+function preload() {
+  campoImg = loadImage('assets/produtos.png');
+  cidadeImg = loadImage('assets/cidade.png');
+  caminhaoImg = loadImage('assets/caminhao.png');
+  somCaminhao = loadSound('assets/som_caminhao.mp3');
+}
 
 function setup() {
-  createCanvas(700, 400);
-  fundo = color(135, 206, 235);  // Azul céu
-  corDoCampo = color(34, 139, 34); // Verde campo
+  createCanvas(800, 400);
 }
 
 function draw() {
-  background(fundo);
+  background(220);
 
-  desenharCampo();
-  desenharCidade();
-  desenharSol();
-  desenharNuvem(mouseX, mouseY, tamanhoNuvem);
-}
+  image(campoImg, 0, 0, width / 2, height);
+  image(cidadeImg, width / 2, 0, width / 2, height);
+  image(caminhaoImg, xCaminhao, height / 2, 80, 50);
 
-function desenharCampo() {
-  fill(corDoCampo);
-  rect(0, height / 2, width, height / 2);
-}
-
-function desenharCidade() {
-  fill(100);
-  for (let i = 0; i < 5; i++) {
-    let predioAltura = random(40, 100);
-    rect(350 + i * 60, height / 2 - predioAltura, 40, predioAltura);
+  if (viagem) {
+    xCaminhao += 2;
+    if (xCaminhao > width) {
+      viagem = false;
+      xCaminhao = 50;
+    }
   }
-}
-
-function desenharSol() {
-  fill(255, 204, 0);
-  ellipse(solX, 80, 60, 60);
-  solX += 0.5;
-  if (solX > width + 30) {
-    solX = -30;
-  }
-}
-
-function desenharNuvem(x, y, tamanho) {
-  fill(255);
-  ellipse(x, y, tamanho, tamanho / 2);
-  ellipse(x + 20, y - 10, tamanho, tamanho / 2);
-  ellipse(x - 20, y - 10, tamanho, tamanho / 2);
 }
 
 function mousePressed() {
-  fundo = color(random(100, 255), random(100, 255), random(100, 255));
-}
-
-function keyReleased() {
-  tamanhoNuvem += 10;
-  if (tamanhoNuvem > 100) {
-    tamanhoNuvem = 50;
+  if (!viagem) {
+    somCaminhao.play();
+    viagem = true;
   }
 }
 
-      
+function keyReleased() {
+  cidadeImg.filter(THRESHOLD);
+}
